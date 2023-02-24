c47460b5213e920c1560f83eb9ce51c5d19115a5
Rafael Souza <rafaelsouza@MacBook-Air-de-Rafael.local>
2023-02-24 15:10:52 -0300

    Exercicios feitos 24/02

---
+++ desafio 3/index.js
@@ -0,0 +1,25 @@
+function review(nota) {
+    if (typeof nota === "number" && nota >= 1 && nota <= 5) {
+        console.log("Sua nota foi ".concat(nota, " estrela(s)"));
+        switch (nota) {
+            case 1:
+                console.log('Sentimos muito :(');
+                break;
+            case 2:
+                console.log('Poxa, seu feedback nos ajuda a melhorar constantemente');
+                break;
+            case 3:
+                console.log('Agradecemos o feedback!');
+                break;
+            case 4:
+                console.log('Que bom :) Volte sempre');
+                break;
+            case 5:
+                console.log('Excelente :D Contamos com seu retorno!');
+        }
+    }
+    else if (typeof nota === 'boolean') {
+        console.log('Nenhuma nota detectada!');
+    }
+}
+review(5);
